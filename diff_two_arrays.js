d9cdf341d3e05a0fa4ff71adee2d2405b8cff02b
jeffbai996 <j.bai@wustl.edu>
2023-03-09 20:27:47 -0800

    Added completed code for Diff Two Arrays

--- diff_two_arrays.js
+++ diff_two_arrays.js
@@ -5,6 +5,18 @@
 
 function diffArray(arr1, arr2) {
   const newArr = [];
+  // iterate over arr1
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      newArr.push(arr1[i]);
+    }
+  }
+  // iterate over arr2
+  for (let j = 0; j < arr2.length; j++) {
+    if (!arr1.includes(arr2[j])) {
+      newArr.push(arr2[j]);
+    }
+  }
   return newArr;
 }
 
