6fcff160beaceb7a0c7ccf9331598de086fdc294
jeffbai996 <j.bai@wustl.edu>
2023-03-10 17:23:43 -0800

    Added first attempt at sumAll() for Sum All Numbers in Range

--- sum_all_in_range.js
+++ sum_all_in_range.js
@@ -4,7 +4,13 @@
 // For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
 
 function sumAll(arr) {
-  return 1;
+  let a = arr[0];
+  let b = arr[1];
+  let sum = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    sum += i;
+  }
+  return sum + a + b;
 }
 
 sumAll([1, 4]);
