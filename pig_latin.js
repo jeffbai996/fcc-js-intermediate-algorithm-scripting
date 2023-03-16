3d9b6365148e64e082ee369163ea2634a4a1f517
jeffbai996 <j.bai@wustl.edu>
2023-03-16 16:40:50 -0700

    Added completed code for Pig Latin

--- pig_latin.js
+++ pig_latin.js
@@ -8,7 +8,28 @@
 // Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
 
 function translatePigLatin(str) {
-  return str;
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  const words = str.toLowerCase().split(" ");
+
+  const pigLatinWords = words.map(word => {
+    let firstVowelIndex = word.length;
+
+    for (let i = 0; i < word.length; i++) {
+      if (vowels.includes(word[i])) {
+        firstVowelIndex = i;
+        break;
+      }
+    }
+
+    if (firstVowelIndex === 0) {
+      return word + "way";
+    } else {
+      return word.slice(firstVowelIndex) + word.slice(0, firstVowelIndex) + "ay";
+    }
+  });
+
+  return pigLatinWords.join(" ");
 }
 
 translatePigLatin("consonant");
