2c2de12c68e7380372d8c84d48d258d1d9f3ec28
jeffbai996 <j.bai@wustl.edu>
2023-03-17 09:30:16 -0700

    Added completed code for Search and Replace

--- search_and_replace.js
+++ search_and_replace.js
@@ -10,7 +10,19 @@
 // Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
 
 function myReplace(str, before, after) {
-  return str;
+  // find index of word to be replaced
+  const i = str.indexOf(before);
+
+  // check case of first char of replaced word
+  if (before[0] === before[0].toUpperCase()) {
+    // update first char of insert word
+    after = after[0].toUpperCase() + after.slice(1);
+  } else {
+    // if replaced word first char not uppercase
+    after = after[0].toLowerCase() + after.slice(1);
+  }
+  // replace word in sentence
+  return str.slice(0, i) + after + str.slice(i + before.length);
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
