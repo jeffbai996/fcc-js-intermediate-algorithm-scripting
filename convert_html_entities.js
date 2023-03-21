77d395bdf9971183709e9ab33bb25cca185f5b01
jeffbai996 <j.bai@wustl.edu>
2023-03-21 10:30:45 -0700

    Add exercise for Convert HTML Entities

---
+++ convert_html_entities.js
@@ -0,0 +1,8 @@
+// Convert HTML Entities
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+function convertHTML(str) {
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
