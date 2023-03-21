513ad3831efefd2becfbda4e251eb33b071c70a3
jeffbai996 <j.bai@wustl.edu>
2023-03-21 10:35:05 -0700

    Add replace for &, <, > for Convert HTML Entities

--- convert_html_entities.js
+++ convert_html_entities.js
@@ -2,6 +2,12 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
 function convertHTML(str) {
+  // replace all '&' with '&amp;'
+  str = str.replace(/&/g, '&amp;');
+  // replace all '<' with '&lt;'
+  str = str.replace(/</g, '&lt;');
+  // replace all '>' with '&gt;'
+  str = str.replace(/>/g, '&gt;');
   return str;
 }
 
