4f3f64bd2c1b7ad2c733cd966707cd1abfdc9f24
jeffbai996 <j.bai@wustl.edu>
2023-03-23 09:05:03 -0700

    Add completed code for Sum All Primes

--- sum_all_primes.js
+++ sum_all_primes.js
@@ -3,8 +3,29 @@
 
 // Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
 
+// declare result
+let sum = 0;
+
+// sum primes using isPrime
 function sumPrimes(num) {
-  return num;
+  let sum = 0;
+  for(let i = 2; i <= num; i++) {
+    if(isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+// check if prime
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 sumPrimes(10);
