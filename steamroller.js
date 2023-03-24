9129907614ddfb8fabfbfff6f4e8d2bf5bc35fd1
jeffbai996 <j.bai@wustl.edu>
2023-03-24 13:17:24 -0700

    Add exercise code for Steamroller

---
+++ steamroller.js
@@ -0,0 +1,8 @@
+// Steamroller
+// Flatten a nested array. You must account for varying levels of nesting.
+
+function steamrollArray(arr) {
+  return arr;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
